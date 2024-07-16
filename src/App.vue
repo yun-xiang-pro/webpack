7bcd4847001269b45cbb294f8db6907f699c400f
yun-xiang-pro <18372107637@163.com>
2024-07-16 08:24:57 +0800

    等比缩放

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,74 @@
 <template>
-    <div>{{nameRef}}</div>
-    <img :src="testJpg"/>
+    <div class="title">{{nameRef}}</div>
+    <!-- <img :src="testJpg"/> -->
+    <div class="imgContainer">
+        <div class="imgItem">
+            <img class="imgel" :src="testJpg"/>
+        </div>
+        <div class="imgItem">
+            <img class="imgel" :src="testJpg"/>
+        </div>
+        <div class="imgItem">
+            <img class="imgel" :src="testJpg"/>
+        </div>
+        <div class="imgItem">
+            <img class="imgel" :src="testJpg"/>
+        </div>
+        <div class="imgItem">
+            <img class="imgel" :src="testJpg"/>
+        </div>
+        <div class="imgItem">
+            <img class="imgel" :src="testJpg"/>
+        </div>
+        <div class="imgItem">
+            <img class="imgel" :src="testJpg"/>
+        </div>
+        <div class="imgItem">
+            <img class="imgel" :src="testJpg"/>
+        </div>
+        <div class="imgItem">
+            <img class="imgel" :src="testJpg"/>
+        </div>
+        <div class="imgItem">
+            <img class="imgel" :src="testJpg"/>
+        </div>
+        <div class="imgItem">
+            <img class="imgel" :src="testJpg"/>
+        </div>
+        <div class="imgItem">
+            <img class="imgel" :src="testJpg"/>
+        </div>
+        <div class="imgItem">
+            <img class="imgel" :src="testJpg"/>
+        </div>
+        <div class="imgItem">
+            <img class="imgel" :src="testJpg"/>
+        </div>
+        <div class="imgItem">
+            <img class="imgel" :src="testJpg"/>
+        </div>
+        <div class="imgItem">
+            <img class="imgel" :src="testJpg"/>
+        </div>
+        <div class="imgItem">
+            <img class="imgel" :src="testJpg"/>
+        </div>
+        <div class="imgItem">
+            <img class="imgel" :src="testJpg"/>
+        </div>
+        <div class="imgItem">
+            <img class="imgel" :src="testJpg"/>
+        </div>
+        <div class="imgItem">
+            <img class="imgel" :src="testJpg"/>
+        </div>
+    </div>
 </template>
 <script setup>
 import { ref} from 'vue';
-import testJpg from '/assets/test.jpg'
+import testJpg from '/assets/test.jpg';
+import './index.css'
 console.log('testJpg:', testJpg)
 
-const nameRef = ref('vue 项目搭建')
+const nameRef = ref('aspect-ratio属性实现等比缩放')
 </script>
